2b8807afd6e4e24cc037de4de996cc75cc0b1cda
IsaqueSeverino <163424817+IsaqueSeverino@users.noreply.github.com>
2025-06-04 15:19:49 -0300

    1.2

--- script.js
+++ script.js
@@ -5,13 +5,9 @@ const amount = document.getElementById('amount');
 
 const expenseList = document.querySelector('ul');
 
-function formatCurrencyBR(value) {
-    value = value.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    })
-    return value
-}
+const expenseTotal = document.querySelector('aside header h2');
+
+const expenseQuantily = document.querySelector('aside header p span');
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, '');
@@ -36,21 +32,29 @@ form.onsubmit = (event) => {
 
 }
 
+function formatCurrencyBR(value) {
+    value = value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    return value
+}
+
 function expenseAdd(newExpense) {
     try {
-        //
+
+        // Cria o item da despesa
         const expenseItem = document.createElement('li');
         expenseItem.classList.add('expense');
 
+
+        // Cria o ícone da despesa
         const expenseIcon = document.createElement('img');
         expenseIcon.setAttribute('src', `img/${newExpense.category_id}.svg`);
         expenseIcon.setAttribute('alt', newExpense.category_name);
 
-        expenseItem.append(expenseIcon, expenseInfo);
 
-        expenseList.append(expenseItem);
-
-        //
+        // Cria o nome da despesa
         const expenseInfo = document.createElement('div');
         expenseInfo.classList.add('expense-info');
 
@@ -62,24 +66,21 @@ function expenseAdd(newExpense) {
 
         expenseInfo.append(expenseName, expenseCategory);
 
-        expenseItem.append(expenseInfo);
 
-        //
-        const expenseAmount = document.createElement('span');
-        
-        expenseAmount.textContent.classList.add('expense-amount');
+        // Cria o valor da despesa
+        const expanseAmount = document.createElement('span');
+        expanseAmount.classList.add('remove-icon');
+        expanseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}` 
 
-        expenseAmount.append(formatCurrencyBR(newExpense.amount));
 
-        expenseItem.append(expenseAmount);
-
-        //
+        // Cria o botão de remover despesa
         const expenseIconRemove = document.createElement('img');
+        expenseIconRemove.classList.add('expense-remove');
         expenseIconRemove.setAttribute('src', 'img/remove.svg');
         expenseIconRemove.setAttribute('alt', 'remover');
 
-        expenseItem.append(expenseIconRemove, expenseInfo);
 
+        expenseItem.append(expenseIcon, expenseInfo, expanseAmount, expenseIconRemove);
         expenseList.append(expenseItem);
 
         updateTotal()
@@ -89,10 +90,45 @@ function expenseAdd(newExpense) {
     }
 }
 
-function updateTotal(){
-    try{
-        const itens = expenseList.children
-    } catch{
+function updateTotal() {
+    try {
+        const items = expenseList.children
+        expenseQuantily.textContent = `${items.length} ${items.length > 1 ? 'itens' : 'item'}`;
+
+        let total = 0;
+
+        for (let item = 0; item < items.length; item++) {
+            const itemAmount = items[item].querySelector('.expense-amount');
+
+            // Remover caracteres não numéricos e substituira a vírgula por ponto
+            let value = itemAmount.textContent.replace(/\D/g, '').replace(',', '.');
+
+            value = parseFloat(value);
+
+            if(isNaN(value)) {
+                return alert("Não foi possível atualizar o total");
+            }
+
+            total += Number(value);
+        }
+
+        // Cria a span para adcionar o R$ formatado
+        const symbolBRL = document.createElement('small');
+        symbolBRL.textContent = 'R$';
+
+        total = formatCurrencyBR(total).toUpperCase().replace('R$', '');
+
+        expenseTotal.innerHTML = ""
+        expenseTotal.append(symbolBRL, total);
+
+    } catch {
         alert("Não foi possível atualizar o total");
     }
 }
+
+expenseList.addEventListener('click', (event) => {
+    if(event.target.classList.contains('expense-remove')) {
+        event.target.closest('.expense').remove();
+        updateTotal();
+    }
+})
